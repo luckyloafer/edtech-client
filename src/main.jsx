c12b03072e1683cb30a1b94b42c412b4192222f6
patnana lakshmana murthy <patnanalakshmanamurthy9@gmail.com>
2024-04-13 14:58:29 +0530

    vercel.json file added, again used app.js fike as routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {RouterProvider} from "react-router-dom";
-import {router} from "./Routes"
+import { HashRouter as Router } from "react-router-dom";
+import { router } from "./Routes"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-<RouterProvider router={router} />
-    {/* <App /> */}
+    {/* <RouterProvider router={router} /> */}
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
