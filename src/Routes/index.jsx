55c4f45b7275fa237d245a588019997b56c98458
patnana lakshmana murthy <patnanalakshmanamurthy9@gmail.com>
2024-04-13 13:26:52 +0530

    test 4

---
+++ src/Routes/index.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
+import SignupComponent from '../components/SignupComponent'
+import HomeLayout from "../layouts/HomeLayout";
+import PaymentSuccess from "../components/PaymentSuccess";
+import PaymentFailure from "../components/PaymentFailure";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login/>,
+    },
+    {
+       path:"/signup",
+       element: <SignupComponent/>
+    },
+    {
+      path:"/home",
+      element: <HomeLayout/>
+   },
+   {
+    path:"/paymentSuccess",
+    element: <PaymentSuccess/>
+ },
+ {
+   path:"/paymentFailure",
+   element: <PaymentFailure/>
+}
+  ]);
